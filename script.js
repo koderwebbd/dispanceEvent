4f0be7dfec9547f0a49d8efc8ca3c690b3151210
koderwebbd <73799573+koderwebbd@users.noreply.github.com>
2024-09-28 04:52:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// const dispanceEvent = new CustomEvent('dispance', {
+// const dispanceEvent = new CustomEvent('search_click', {
 //   detail: { message: 'This is a custom dispance event' }
 // });
 const dispanceEvent = new CustomEvent('search_click');
@@ -8,4 +8,14 @@ document.addEventListener('search_click', function(event) {
       //console.log('search_click event triggeredx:', event.detail.message);
 });
 
- 
+//another example 
+const dispanceEvent = new CustomEvent('variant_click', {
+detail: { variant: selectedVariant }
+});
+//const dispanceEvent = new CustomEvent('variant_click');
+document.dispatchEvent(dispanceEvent);
+
+//receive
+document.addEventListener('variant_click', (event) => { 
+    console.log(event.detail.variant)
+});
